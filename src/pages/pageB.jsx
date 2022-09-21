79c4137caa389aaa324bdf01769cf66c338fc463
NickYang <e102777tw@gmail.com>
2022-09-21 22:43:14 +0800

    profile basic

--- src/pages/pageB.jsx
+++ src/pages/pageB.jsx
@@ -1,16 +1,5 @@
 import React from "react";
-import { Tabs } from 'antd';
-const items = [
-  { label: 'Tab 1', key: 'item-1', children: 'Content 1' }, // remember to pass the key prop
-  { label: 'Tab 2', key: 'item-2', children: 'Content 2' },
-];
-export default function PageB() {
-  return <Tabs>
-  <Tabs.TabPane tab="Tab 1" key="item-1">
-    Content 1
-  </Tabs.TabPane>
-  <Tabs.TabPane tab="Tab 2" key="item-2">
-    Content 2
-  </Tabs.TabPane>
-</Tabs>;
+
+export default function PageA() {
+  return <p>PageB</p>;
 }
