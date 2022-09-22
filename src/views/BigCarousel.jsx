144043cf4abb4c4f1e20b4ef35e228ffe13bc32f
ek2061 <jackey860517@gmail.com>
2022-09-22 22:55:59 +0800

    fix(homepage): fixed carousel width issue

--- src/views/BigCarousel.jsx
+++ src/views/BigCarousel.jsx
@@ -1,26 +1,23 @@
-import { Carousel } from 'antd'
-import React from 'react'
-import { LeftOutlined, RightOutlined } from '@ant-design/icons'
-import { useRef } from 'react'
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import { Carousel } from "antd";
+import React from "react";
+
+const contentStyle = {
+  height: "160px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79",
+};
 
 export const BigCarousel = () => {
-  const contentStyle = {
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-  }
-  const ref = useRef()
+  const carouselRef = React.useRef();
+  const onLeftArrow = () => carouselRef.current.prev();
+  const onRightArrow = () => carouselRef.current.next();
+
   return (
-    <div style={{ display: 'flex ', flexDirection: 'row' ,justifyContent:'center' ,alignItems:'center'}}>
-      <LeftOutlined
-        style={{ fontSize: '32px' }}
-        onClick={() => {
-          ref.current.prev()
-        }}
-      />
-      <Carousel dots={false} draggable slidesToShow={2} ref={ref} style={{width:1000}}>
+    <div style={{ width: "100%", position: "relative" }}>
+      <Carousel dots={false} draggable slidesToShow={2} ref={carouselRef}>
         <div>
           <h3 style={contentStyle}>1</h3>
         </div>
@@ -34,12 +31,26 @@ export const BigCarousel = () => {
           <h3 style={contentStyle}>4</h3>
         </div>
       </Carousel>
-      <RightOutlined
-        style={{ fontSize: '32px' }}
-        onClick={() => {
-          ref.current.next()
+      <LeftOutlined
+        style={{
+          fontSize: "32px",
+          display: "flex",
+          position: "absolute",
+          top: "64px",
+          left: "0",
         }}
+        onClick={onLeftArrow}
+      />
+      <RightOutlined
+        style={{
+          fontSize: "32px",
+          display: "flex",
+          position: "absolute",
+          top: "64px",
+          right: "0",
+        }}
+        onClick={onRightArrow}
       />
     </div>
-  )
-}
+  );
+};
