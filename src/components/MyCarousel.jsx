5d34ee6db806522ddb04809691574e752808986e
jumpjet0524 <jumpjet0524@gmail.com>
2022-10-23 23:01:29 +0800

    perf: ⚡️ remove useRef

--- src/components/MyCarousel.jsx
+++ src/components/MyCarousel.jsx
@@ -2,6 +2,8 @@ import { RightCircleOutlined, LeftCircleOutlined } from "@ant-design/icons";
 import { Carousel } from "antd";
 import React from "react";
 
+import "./MyCarousel.css";
+
 const contentStyle = {
   height: "250px",
   color: "#fff",
@@ -14,39 +16,21 @@ const contentStyle = {
 
 export const MyCarousel = (props) => {
   const carouselRef = React.useRef(); //1.宣告一個ref物件
-  const onLeftArrow = () => carouselRef.current.prev(); //3.current使用方法
-  const onRightArrow = () => carouselRef.current.next();
 
   return (
     <div>
-      <div style={{ position: "relative" }}>
+      <div>
         <Carousel
+          arrows
+          nextArrow={<RightCircleOutlined />}
+          prevArrow={<LeftCircleOutlined />}
           dots={false}
           draggable
           slidesToShow={props.shownums}
-          ref={carouselRef}
         >
           {/*2.將ref物件指向Carousel*/}
           {props.views}
         </Carousel>
-        <LeftCircleOutlined
-          style={{
-            fontSize: "50px",
-            position: "absolute",
-            top: "calc(50% - 25px)",
-            left: "-15px",
-          }}
-          onClick={onLeftArrow}
-        />
-        <RightCircleOutlined
-          style={{
-            fontSize: "50px",
-            position: "absolute",
-            top: "calc(50% - 25px)",
-            right: "-15px",
-          }}
-          onClick={onRightArrow}
-        />
       </div>
     </div>
   );
--- src/pages/pageA.jsx
+++ src/pages/pageA.jsx
@@ -5,19 +5,22 @@ import { DailyPicks } from "@/views/DailyPicks";
 
 export default function PageA() {
   return (
-    <div
-      style={{
-        width: "90%",
-        margin: "auto",
-      }}
-    >
-      <BigCarousel />
-      <h2>探索Carousell</h2>
-      <SamllCarousel />
-      <h2>你的每日精選</h2>
-      <DailyPicks />
-      <h2>為你推薦</h2>
-      <Recommend />
+    <div>
+      <div>123</div>
+      <div
+        style={{
+          width: "90%",
+          margin: "auto",
+        }}
+      >
+        <BigCarousel />
+        <h2>探索Carousell</h2>
+        <SamllCarousel />
+        <h2>你的每日精選</h2>
+        <DailyPicks />
+        <h2>為你推薦</h2>
+        <Recommend />
+      </div>
     </div>
   );
 }
